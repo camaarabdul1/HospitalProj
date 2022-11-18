56887f7391cb4d514fd03f13fb8914d39f407043
camabdulkadir <camaarabdul32@gmail.com>
2022-11-18 09:45:45 -0600

    initial commit

---
+++ src/Doctor.java
@@ -0,0 +1,35 @@
+public class Doctor {
+    String name;
+    String phoneNumber;
+    String speciality;
+
+    public Doctor(String name, String phoneNumber, String speciality) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.speciality = speciality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+}
--- src/Patient.java
+++ src/Patient.java
@@ -1,3 +1,47 @@
 public class Patient {
+    String name;
+    String phoneNumber;
+    String description;
+    int age;
+
+    public Patient(String name, String phoneNumber, String description, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.description = description;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 
 }
